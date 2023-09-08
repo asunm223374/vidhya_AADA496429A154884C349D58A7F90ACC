5fb71573803416348fe75a33c2c2f9ee71e7d0f3
asunm223374 <144433757+asunm223374@users.noreply.github.com>
2023-09-08 14:35:29 +0530

    Add files via upload

---
+++ challenge unit1/Leap Year Coding.py
@@ -0,0 +1,11 @@
+#Program to check a year is leap year or not
+def isLeapYear(year):
+  if (year % 4)==0 and (year % 100)!=0 or (year % 400)==0:
+    return True
+  else:
+    return False
+year=(int(input("Enter a Year:")))
+if isLeapYear(year):
+  print("{} is leap year".format(year))
+else:
+  print("{} is not a leap year".format(year))
